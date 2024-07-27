09af1dc13334138ee761745af122e8833f84fdf7
ashishdubey195 <68372389+ashishdubey195@users.noreply.github.com>
2024-07-27 18:45:31 +0530

    Update main.tf

--- TERRAFORM-SCRIPTS/main.tf
+++ TERRAFORM-SCRIPTS/main.tf
@@ -6,14 +6,6 @@ terraform {
     }
   }
 }
-
-provider "azurerm" {
-  features {
-    resource_group {
-       prevent_deletion_if_contains_resources = false
-     }
-  }
-}
 provider "azurerm" {
    features {
    resource_group {

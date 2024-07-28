647bf55b183011f7504b54647f95666d9f24363b
ashishdubey195 <68372389+ashishdubey195@users.noreply.github.com>
2024-07-28 18:31:10 +0530

    Update main.tf

--- TERRAFORM-SCRIPTS/main.tf
+++ TERRAFORM-SCRIPTS/main.tf
@@ -13,36 +13,6 @@ provider "azurerm" {
   }
  }
 }
-variable "resource_group_name" {
-    type = string
-    description = "RG in azure"
-}
-
-variable "virtual_network_name" {
-    type = string
-    description = "VG in azure"
-}
-variable "subnet_name" {
-    type = string
-    description = "subnet in azure"
-}
-variable "network_interface_name" {
-    type = string
-    description = "network interface name in azure"
-}
-variable "public_ip_name" {
-    type = string
-    description = "Public IP in azure"
-}
-variable "virtual_machine_name" {
-    type = string
-    description = "Virtual machine in azure"
-}
-variable "computer_names" {
-  description = "Names of the computers for each environment"
-  type        = list(string)
-  default     = ["dev-env", "int-env", "prod-env"]
-}
 
 resource "azurerm_resource_group" "myrg1" {
   name     = "var.resource_group_name"
